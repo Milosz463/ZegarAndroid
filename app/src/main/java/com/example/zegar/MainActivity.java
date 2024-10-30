c8473b40d70537de9ab8a698e531cea33d3e656f
Milosz463 <miloszdawiec6@gmail.com>
2024-10-30 13:39:38 +0100

    przerwa

--- app/src/main/java/com/example/zegar/MainActivity.java
+++ app/src/main/java/com/example/zegar/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.zegar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttongracz1;
+    Button buttongracz2;
+    Gracz gracz1;
+    Gracz gracz2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttongracz1=findViewById(R.id.button);
+        buttongracz2=findViewById(R.id.button2);
+        gracz1=new Gracz(true,buttongracz1);
+        gracz2=new Gracz(false,buttongracz2);
+        buttongracz1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(gracz1.czyDziala){
+                            gracz1.stopTimer();
+                            gracz2.startTimer();
+                        }
+                    }
+                }
+        );
+        buttongracz2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(gracz2.czyDziala){
+                            gracz2.stopTimer();
+                            gracz1.startTimer();
+                        }
+                    }
+                }
+        );
     }
 }
